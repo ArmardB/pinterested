74290a72319de146d26428cfd480a12594a2853c
Armard Bellamy <armardbellamy@gmail.com>
2016-08-08 06:54:03 -0400

    Updated posts controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -45,7 +45,7 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    def correct_user
+    def current_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin." if @pin.nil?
     end
